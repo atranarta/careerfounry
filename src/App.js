5c2e29c28fbad5d86c2bb4039aeec1b82fbca434
atranarta <radyuk.tatyana@gmail.com>
2020-11-01 16:13:54 +0100

    Created component Detalils

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Courses from "./components/Courses/Courses";
+import Details from "./components/Details/Details";
 
 function App() {
   return (
     <div className="app">
       <main>
         <Courses />
+        <Details />
       </main>
     </div>
   );
---
+++ src/components/Details/Details.js
@@ -0,0 +1,20 @@
+const Details = () => {  
+  return (
+    <div className="details">
+      <div className="description">
+        <h1>The voice user interface design course with Amazon Alexa</h1>
+        <div className="start-dates">Next dates: 
+          <ul>
+            <li>2020-08-17</li>
+            <li>2020-09-17</li>
+            <li>2020-10-17</li>
+          </ul>
+        </div>
+        <div className="price-container"> Price: 1800 eur</div>
+      </div>
+    </div>
+  )
+};
+
+
+export default Details;
