a6d7173590dcb80daab78495a6cf18294a3ce040
atranarta <radyuk.tatyana@gmail.com>
2020-11-01 18:23:28 +0100

    Display of fetched content

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,30 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import studentImg from '../../images/Abhi-1279-min.png';
 
 import './details.scss';
 
 const Details = () => {
+  const [details, setDetails] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function getDetails() {
+      setIsLoading(true);
       const courseDetail = await fetch(`https://private-e05942-courses22.apiary-mock.com/courses/full-stack-immersion`);
       const data = await courseDetail.json();
-      console.log(data);
+      setDetails([data]);
+      setIsLoading(false);
     }
     getDetails();
   }, []);
 
+  if (isLoading) { 
+    return (
+      <div className="preloader">Data loading...</div>
+    );
+  }
 
   return (
     <div className="details">
       <div className="description">
-        <h1>The voice user interface design course with Amazon Alexa</h1>
+        <h1>{details[0].description}</h1>
         <div className="start-dates">Next dates:
           <ul>
-            <li>2020-08-17</li>
-            <li>2020-09-17</li>
-            <li>2020-10-17</li>
+            {details[0].start_dates.map((date) => <li>{date}</li>)}
           </ul>
         </div>
         <div className="price-container">Price:
